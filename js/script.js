47bdd53611133ad57b73254467d4b83705739c50
gl-guccione <g.guccione3@gmail.com>
2020-08-04 16:50:00 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -7,8 +7,21 @@
 
 var km = prompt("Inserire km da percorrere"); // getting km
 var age = prompt("Inserire l'età"); // getting age
+// console.log(km + " " + age);
 
 var priceKm = 0.21; // base price
+
+
+// error if km or age NaN
+if (isNaN(km)) {
+  alert("Errore, i dati inseriti non sono validi");
+  km = prompt("Inserire km da percorrere");
+} else if (isNaN(age)) {
+  alert("Errore, i dati inseriti non sono validi");
+  age = prompt("Inserire l'età");
+}
+// console.log(km + " " + age);
+
 
 if (age < 18) { // get 20% discount
   priceKm = priceKm * 0.8;
@@ -17,6 +30,7 @@ if (age < 18) { // get 20% discount
 }
 
 var totalPrice = priceKm * km; // getting the price
+totalPrice = totalPrice.toFixed(2);
 
 // printing results in HTML
-document.getElementById("total-price").innerHTML = totalPrice;
+document.getElementById("total-price").innerHTML = totalPrice + "€";
