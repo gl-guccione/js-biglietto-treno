0be6899bfd4b0ec0a64c8d09ce118d7d7ab2a876
gl-guccione <g.guccione3@gmail.com>
2020-08-09 11:52:10 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -9,9 +9,9 @@ var name = prompt("Inserire nome"); // getting name
 var surname = prompt("Inserire cognome"); // getting surname
 var km = prompt("Inserire km da percorrere"); // getting km
 var age = prompt("Inserire l'età"); // getting age
-var sconto; // declare sconto
+var discount; // declare discount
 var d = new Date(); // getting date from user's pc
-var domani = d.getDate() + 1; // calculate tomorrow
+var tomorrow = d.getDate() + 1; // calculate tomorrow
 // console.log(km + " " + age);
 
 var priceKm = 0.21; // base price
@@ -30,12 +30,12 @@ if (isNaN(km)) {
 
 if (age < 18) { // get 20% discount
   priceKm = priceKm * 0.8;
-  sconto = "20";
-  document.getElementById("sconto").innerHTML = "* sconto già applicato del " + sconto + "%";
+  discount = "20";
+  document.getElementById("discount").innerHTML = "* sconto già applicato del " + discount + "%";
 } else if (age >= 65) { // get 40% discount
   priceKm = priceKm * 0.6;
-  sconto = "40"
-  document.getElementById("sconto").innerHTML = "* sconto già applicato del " + sconto + "%";
+  discount = "40"
+  document.getElementById("discount").innerHTML = "* sconto già applicato del " + discount + "%";
 }
 
 var totalPrice = priceKm * km; // getting the price
@@ -47,4 +47,4 @@ document.getElementById("name").innerHTML = name;
 document.getElementById("surname").innerHTML = surname;
 document.getElementById("km").innerHTML = km + "km";
 document.getElementById("clock").innerHTML = d.getDate() + "-" + d.getMonth() + "-" + d.getFullYear() + " " + d.getHours() + ":" + d.getMinutes();
-document.getElementById("date").innerHTML = domani + "-" + d.getMonth() + "-" + d.getFullYear();
+document.getElementById("date").innerHTML = tomorrow + "-" + d.getMonth() + "-" + d.getFullYear();
